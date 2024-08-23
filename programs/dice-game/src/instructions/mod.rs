7ff8ab8a6cec476c00b7f0f5074279ac982f16c6
Sam Miller <sammiller85@protonmail.com>
2024-08-23 14:50:18 -0700

    first commit - initialize and place_bet functions done

---
+++ programs/dice-game/src/instructions/mod.rs
@@ -0,0 +1,8 @@
+pub mod initialize;
+pub use initialize::*;
+
+pub mod place_bet;
+pub use place_bet::*;
+
+pub mod resolve_bet;
+pub use resolve_bet::*;
---
+++ programs/dice-game/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod errors;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("8nkxMNBPwGobzFPjXLgK7yM4eYYfzK4V3ZKLbojehFkW");
+
+#[program]
+pub mod dice_game {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
